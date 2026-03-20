9e1ba3481ed104466cb4fd8b148a771b48c62c5d
kana-shii <79055104+kana-shii@users.noreply.github.com>
2026-03-19 21:04:37 -0300

    Update MangaBakaApi.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/MangaBakaApi.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/MangaBakaApi.kt
@@ -361,7 +361,7 @@ class MangaBakaApi(
             body = FormBody.Builder()
                 .add("grant_type", "refresh_token")
                 .add("client_id", CLIENT_ID)
-                .add("refreshToken", token)
+                .add("refresh_token", token)
                 .add("redirect_uri", REDIRECT_URI)
                 .build(),
         )
